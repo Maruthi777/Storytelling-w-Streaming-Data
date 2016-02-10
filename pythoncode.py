88d7c809a77430ad56c4cad07f47f3a65646a46e
Maruthi Vemuri <msv2130@columbia.edu>
2016-02-10 17:40:09 -0500

    python code for the application

--- pythoncode.py
+++ pythoncode.py
@@ -15,11 +15,11 @@ while True:
 	obj = json.loads(response.read())
         lat= obj['iss_position']['latitude']#store latitude position
         lon= obj['iss_position']['longitude']   # store longitude position
-        g = geocoder.google([float(lat), float(lon)], method='reverse')
-        if g.country_long == None:
+        g = geocoder.google([float(lat), float(lon)], method='reverse') #to locate country/international waters based on latitude and longitude
+        if g.country_long == None: #if no country, then international waters
                 loc="International Waters"
         else:
-                loc=g.country_long
+                loc=g.country_long #else print country name
         print(loc)      
 	print obj['timestamp'] #print time when latitude and longitude are recorded
         print '{"latitude is":"%s","longitude is":"%s"}'%(lat, lon) #print latitude and longitude
